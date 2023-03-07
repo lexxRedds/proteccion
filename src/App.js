2f23df30aba14565f8b6e63a08be1a6b002055d9
Lexx R <alexcumbal@gmail.com>
2023-03-07 10:22:01 -0500

    Udated with calculating the series

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+
+import Home from './components/Home';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(new Date());
-  const [timeArray, setTimeArray] = useState([]);
-
-  function updateTime() {
-    setInterval(() => setCurrentTime(new Date()), 1000);
-  }
-
-  function generateFibonacci() {
-    setTimeArray([...timeArray, currentTime.toLocaleTimeString()]);
-  }
-
-  useEffect(() => {
-    updateTime();
-  }, []);
 
   return (
     <div className="App">
-      <header className="App-header">
-        {/*<img src={logo} className="App-logo" alt="logo" />*/}
-        <h1>Current Time:</h1>
-        <h2>{currentTime.toLocaleTimeString()}</h2>
-        <button onClick={generateFibonacci}>Generar Serie</button>
-        
-      </header>
+      <Home />
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,53 @@
+import React, { useState, useEffect } from 'react';
+
 function Home() {
-    return (
-        <div className="home-main ">
-            <Banner />
-            <Content />
-            <Info />
-            <Newsletter />
-        </div>
-    )
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [timeArray, setTimeArray] = useState([]);
+  const [fibo, setFibo] = useState([]);
+
+  function updateTime() {
+    setInterval(() => setCurrentTime(new Date()), 1000);
+  }
+
+  function handleClick() {
+    const time = currentTime.toLocaleTimeString();
+    const [hours, minutes, seconds] = time.split(':');
+
+    const minute1 = Math.floor(minutes / 10);
+    const minute2 = minutes % 10;
+    let fibo = [minute1, minute2];
+
+    for (let i = 2; i < seconds; i++) {
+      fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+
+    setFibo(fibo);
+
+        
+    const sum = parseInt(minutes, 10) + parseInt(seconds, 10);
+    setTimeArray([...timeArray, sum]);
+
+  }
+
+  useEffect(() => {
+    updateTime();
+  }, []);
+
+  return (
+    <div>
+      <h1>Hora Actual</h1>
+      <div>{currentTime.toLocaleTimeString()}</div>
+      <button onClick={handleClick}>Generar Serie</button>
+      <div>
+        <h1>Serie de Fibonacci</h1>
+        <ul>
+          {fibo.map((value, index) => (
+            <li key={index}>{value}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
