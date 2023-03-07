0e98e5b6a02959555609bac43e48855e4a936b41
Lexx R <alexcumbal@gmail.com>
2023-03-07 09:37:39 -0500

    Show time

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const [timeArray, setTimeArray] = useState([]);
+
+  function updateTime() {
+    setInterval(() => setCurrentTime(new Date()), 1000);
+  }
+
+  function generateFibonacci() {
+    setTimeArray([...timeArray, currentTime.toLocaleTimeString()]);
+  }
+
+  useEffect(() => {
+    updateTime();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <h1>Current Time:</h1>
+        <h2>{currentTime.toLocaleTimeString()}</h2>
+        <button onClick={generateFibonacci}>Generar Serie</button>
+        
       </header>
     </div>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+function Home() {
+    return (
+        <div className="home-main ">
+            <Banner />
+            <Content />
+            <Info />
+            <Newsletter />
+        </div>
+    )
+}
+
+export default Home;
